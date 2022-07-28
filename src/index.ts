22262baa355b91c523ef9f354e58eaece19b3b33
foxnorth228 <khitrii03@gmail.com>
2022-07-28 16:51:34 +0300

    feat: add pagination and start page view

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,36 @@
 import "./main.css";
+type ElemOrNull = Element | null;
+
+const startPageSwitch: ElemOrNull = document.querySelector(".startPageSwitch");
+const toyPageSwitch: ElemOrNull = document.querySelector(".toyPageSwitch");
+const treePageSwitch: ElemOrNull = document.querySelector(".treePageSwitch");
+const startPage: ElemOrNull = document.querySelector(".startPage");
+const toyPage: ElemOrNull = document.querySelector(".toyPage");
+const treePage: ElemOrNull = document.querySelector(".treePage");
+let currentPage: ElemOrNull = startPage;
+
+startPageSwitch?.addEventListener("click", function() {
+    changePage(startPage);
+});
+toyPageSwitch?.addEventListener("click", function() {
+    changePage(toyPage);
+});
+treePageSwitch?.addEventListener("click", function() {
+    changePage(treePage);
+});
+
+function changePage(oldPage: ElemOrNull) {
+    if (currentPage === oldPage || oldPage === null) {
+        return;
+    }
+    currentPage?.classList.add("pageDisNone");
+    currentPage?.classList.remove("pageDisFlex");
+    currentPage = oldPage;
+    currentPage?.classList.remove("pageDisNone");
+    currentPage?.classList.add("pageDisFlex");
+}
+
+const startPageButton = document.querySelector(".startPageButton");
+startPageButton?.addEventListener("click", function() {
+    changePage(toyPage);
+})
