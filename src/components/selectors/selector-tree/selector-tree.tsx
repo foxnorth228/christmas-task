14364dc8ab2694548d7e21585b984b115001db3c
Alex Khitry <khitrii03@gmail.com>
2023-07-19 02:36:36 +0300

    feat: implement selector tree

--- src/components/selectors/selector-tree/selector-tree.tsx
+++ src/components/selectors/selector-tree/selector-tree.tsx
@@ -2,7 +2,22 @@ import React from 'react';
 import './selector-tree.scss';
 
 const SelectorTree = () => {
-  return <div></div>;
+  const fileNames = ['1', '2', '3', '4', '5', '6'];
+  return (
+    <div className="selectorTree">
+      <h2 className="selectorTree__title">Выберите ёлочку</h2>
+      <div className="selectorTree__elements">
+      {fileNames.map((el, i) => (
+        <div key={i} className="selectorTree__element">
+          <div
+            style={{ backgroundImage: `url("./tree/${el}.png")` }}
+            className="selectorTree__example"
+          ></div>
+        </div>
+      ))}
+      </div>
+    </div>
+  );
 };
 
 export default SelectorTree;
