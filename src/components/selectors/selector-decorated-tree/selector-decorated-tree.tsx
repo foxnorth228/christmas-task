33e80d056c6bca2c4104f7b63126e0dbad894ed5
Alex Khitry <khitrii03@gmail.com>
2023-07-20 01:06:07 +0300

    feat: implement selector dec tree

--- src/components/selectors/selector-decorated-tree/selector-decorated-tree.tsx
+++ src/components/selectors/selector-decorated-tree/selector-decorated-tree.tsx
@@ -2,7 +2,26 @@ import React from 'react';
 import './selector-decorated-tree.scss';
 
 const SelectorDecoratedTree = () => {
-  return <div></div>;
+  const fileNames = ['1', '2', '3', '4',]
+  return (
+    <div className="selectorDecTree">
+      <h2 className="selectorDecTree__title">Вы нарядили</h2>
+      <div className="selectorDecTree__slider">
+        <div className="selectorDecTree__elements">
+          {fileNames.map((el, i) => (
+            <div key={i} className="selectorDecTree__element">
+              <div
+                style={{ backgroundImage: `url('./tree/${el}.png')` }}
+                className="selectorDecTree__example"
+              ></div>
+            </div>
+          ))}
+        </div>
+        <div className="selectorDecTree__arrow selectorDecTree__arrow_left"></div>
+        <div className="selectorDecTree__arrow selectorDecTree__arrow_right"></div>
+      </div>
+    </div>
+  );
 };
 
 export default SelectorDecoratedTree;
