915ec473720617b18e7a0e0b3bece35f3c875393
Alex Khitry <khitrii03@gmail.com>
2023-07-20 00:28:35 +0300

    feat: implement selector garland

--- src/components/selectors/selector-garland/selector-garland.tsx
+++ src/components/selectors/selector-garland/selector-garland.tsx
@@ -2,7 +2,17 @@ import React from 'react';
 import './selector-garland.scss';
 
 const SelectorGarland = () => {
-  return <div></div>;
+  const garlands = ['multicolor', 'red', 'blue', 'yellow', 'green'];
+  return (
+    <div className="selectorGarland">
+      <h2 className="selectorGarland__title">Гирлянда</h2>
+      <div className="selectorGarland__elements">
+        {garlands.map((el, i) => (
+          <div className={`selectorGarland__element selectorGarland__element_${el}`}></div>
+        ))}
+      </div>
+    </div>
+  );
 };
 
 export default SelectorGarland;
