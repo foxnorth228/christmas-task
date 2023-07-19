3cafceea82e0b63ed0472ec22e298909b4b597d4
Alex Khitry <khitrii03@gmail.com>
2023-07-20 00:48:32 +0300

    feat: implement selector toys

--- src/components/selectors/selector-toys/selector-toys.tsx
+++ src/components/selectors/selector-toys/selector-toys.tsx
@@ -2,7 +2,22 @@ import React from 'react';
 import './selector-toys.scss';
 
 const SelectorToys = () => {
-  return <div></div>;
+  const fileNames: number[] = [];
+  for (let i = 0; i < 20; ++i) {
+    fileNames.push(i + 1);
+  }
+  return (
+    <div className="selectorToys">
+      <h2 className="selectorToys__title">Игрушки</h2>
+      <div className="selectorToys__elements">
+        {fileNames.map((el) => (
+          <div key={el} className="selectorToys__element">
+            <div style={{ backgroundImage: `url('./toys/${el}.png')` }} className="selectorToys__example"></div>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
 };
 
 export default SelectorToys;
