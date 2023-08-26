575878bd62e1b16db898ce1a7380fbcd7ceb5ed9
Alex Khitry <khitrii03@gmail.com>
2023-08-26 23:08:24 +0300

    feat: implement snow falling

--- src/components/snowflake/snowflake.tsx
+++ src/components/snowflake/snowflake.tsx
@@ -2,15 +2,20 @@ import React, { useRef } from 'react';
 import './snowflake.scss';
 
 const Snowflake = () => {
+  const posLeft = useRef(Math.random() * 100);
   const type = useRef(Math.ceil(Math.random() * 6));
   return (
-    <div
-      style={{
-        maskImage: `url("./snow/snow-${type.current}.svg")`,
-        WebkitMaskImage: `url("./snow/snow-${type.current}.svg")`,
-      }}
-      className="snowflake"
-    ></div>
+    <div className="snowflake__container_y" style={{ left: `${posLeft.current}%` }}>
+      <div className="snowflake__container_x">
+        <div
+          style={{
+            maskImage: `url("./snow/snow-${type.current}.svg")`,
+            WebkitMaskImage: `url("./snow/snow-${type.current}.svg")`,
+          }}
+          className="snowflake"
+        ></div>
+      </div>
+    </div>
   );
 };
 
--- src/layouts/snowfall/snowfall.tsx
+++ src/layouts/snowfall/snowfall.tsx
@@ -2,8 +2,15 @@ import React from 'react';
 import './snowfall.scss';
 import Snowflake from '@components/snowflake/snowflake';
 
+export interface IAnimation {
+  duration: number;
+  easing: string;
+  iterations: number;
+  steps: string[];
+}
+
 const Snowfall = () => {
-  const count = 20;
+  const count = 100;
   const countArr = new Array(count).fill(0);
   return (
     <section className="snowfall">
