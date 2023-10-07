a829f0f236f8d1586ef43aeb829a436733636474
Alex Khitry <khitrii03@gmail.com>
2023-10-08 00:03:48 +0300

    fix: fix rendering element dublicating

--- src/index.tsx
+++ src/index.tsx
@@ -4,6 +4,7 @@ import './index.scss';
 import { RouterProvider } from 'react-router-dom';
 import router from '@routes/route';
 
+console.log('root')
 const rootNode = document.getElementById('root');
 if (rootNode) {
   createRoot(rootNode).render(<RouterProvider router={router} />);
