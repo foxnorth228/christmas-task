12040361a96ae13db26b011d3d42073f41e86cc3
Alex Khitry <khitrii03@gmail.com>
2023-10-09 19:19:17 +0300

    fix: some other minor fixes

--- src/pages/tree-page/tree-page.tsx
+++ src/pages/tree-page/tree-page.tsx
@@ -20,6 +20,7 @@ function TreePage() {
     refToy.current.style.top = activeToy.y + 'px';
   }, [activeToy]);
   const touchmove = (e: React.TouchEvent<HTMLDivElement>) => {
+    e.stopPropagation();
     if (refToy.current === null) {
       return;
     }
