1635dac751f786a4652af691f4b2cc1d0e748393
Alex Khitry <khitrii03@gmail.com>
2023-10-21 23:13:30 +0300

    init: init firework

---
+++ src/layouts/Firework/index.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './style.scss';
+
+const Firework = () => {
+  return (
+    <div className="firework">
+      <div className="firework__missile"></div>
+    </div>
+  );
+};
+
+export default Firework;
